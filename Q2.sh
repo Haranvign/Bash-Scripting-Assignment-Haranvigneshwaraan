4b29e3d3d60c34167b1ba41d62666289c7a324c3
Haranvign <100628546+Haranvign@users.noreply.github.com>
2022-03-13 19:54:06 -0400

    Update Q2.sh

--- Q2.sh
+++ Q2.sh
@@ -1,11 +1,12 @@
-printf "Kernel Version:  `uname -r`\n";
-printf "Bash Version: ${BASH_VERSION}\n"
-printf "Free Storage (mounted): `df -h $PWD | awk '/[0-9]%/{print $(NF-2)}'` \n";
+BDAY="04/01/1999";
+echo $BDAY;
 
-mem_Kb=$(grep -i memfree /proc/meminfo | cut -d: -f2 | tr -d [kB] | sed "s/^[ \t]*// ; s/[ \t]*$//")
-mem_Gig=$(echo "scale = 1 ; $mem_Kb / 1000000" | bc )
-echo Free Memory: $mem_Gig GB
+SUM=0
+num_regex='^[0-9]+$'
+for (( i=0; i<${#BDAY}; i++ )); do
+    if [[ ${BDAY:$i:1} =~ $num_regex ]] ; then
+        SUM=`expr $SUM + ${BDAY:$i:1}`
+    fi
+done
 
-echo "Files in directory: `ls | wc -l`";
-echo "IP Address: `hostname -I | awk '{print $1;}'`";
-echo "Active Interfaces: `ip addr show | awk '/inet.*brd/{print $NF}'`";
+echo $SUM
